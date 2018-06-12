5d822b1518c86022bf38271a110aaf856510023e
Diego Osmarin Basso <diegoobasso@outlook.com>
2018-06-12 10:20:42 -0300

    Teste integração contínua

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({Arquiteto: "Israel Deorce Vieira Junior",
+            Frontend: "Hercílio Ortiz Martins",
+            Backend: "Diego Osmarin Basso"});
 });
 
 module.exports = router;
