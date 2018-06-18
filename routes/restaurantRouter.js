ebf05459f188722fa72089269d506f570d357025
DiegoOsmarinBasso <diego.osmarin@acad.pucrs.br>
2018-06-18 10:43:37 -0300

    route /products

--- routes/restaurantRouter.js
+++ routes/restaurantRouter.js
@@ -16,8 +16,8 @@ restaurantRouter.route("/")
     Restaurant.find({})
     .then((restaurants) => {
         restaurants.forEach(restaurant => {
-            restaurant.createdAt = restaurant.createdAt - GMT_Brasil;
-            restaurant.updatedAt = restaurant.updatedAt - GMT_Brasil; 
+            restaurant.createdAt -= GMT_Brasil;
+            restaurant.updatedAt -= GMT_Brasil;
         });
 
         res.statusCode = 200;
@@ -29,8 +29,8 @@ restaurantRouter.route("/")
 .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Restaurant.create(req.body)
     .then((restaurant) => {
-        restaurant.createdAt = restaurant.createdAt - GMT_Brasil;
-        restaurant.updatedAt = restaurant.updatedAt - GMT_Brasil;
+        restaurant.createdAt -= GMT_Brasil;
+        restaurant.updatedAt -= GMT_Brasil;
         
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
@@ -59,8 +59,8 @@ restaurantRouter.route("/:restaurantId")
 .get((req, res, next) => {
     Restaurant.findById(req.params.restaurantId)
     .then((restaurant) => {
-        restaurant.createdAt = restaurant.createdAt - GMT_Brasil;
-        restaurant.updatedAt = restaurant.updatedAt - GMT_Brasil;
+        restaurant.createdAt -= GMT_Brasil;
+        restaurant.updatedAt -= GMT_Brasil;
         
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
@@ -77,8 +77,8 @@ restaurantRouter.route("/:restaurantId")
         $set: req.body
     }, { new: true })
     .then((restaurant) => {
-        restaurant.createdAt = restaurant.createdAt - GMT_Brasil;
-        restaurant.updatedAt = restaurant.updatedAt - GMT_Brasil;
+        restaurant.createdAt -= GMT_Brasil;
+        restaurant.updatedAt -= GMT_Brasil;
 
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
@@ -90,8 +90,8 @@ restaurantRouter.route("/:restaurantId")
     Restaurant.findByIdAndRemove(req.params.restaurantId)
     .then((restaurant) => {
         if(restaurant != null) {
-            restaurant.createdAt = restaurant.createdAt - GMT_Brasil;
-            restaurant.updatedAt = restaurant.updatedAt - GMT_Brasil;
+            restaurant.createdAt -= GMT_Brasil;
+            restaurant.updatedAt -= GMT_Brasil;
             
             res.statusCode = 200;
             res.setHeader("Content-Type", "application/json");
@@ -109,6 +109,161 @@ restaurantRouter.route("/:restaurantId")
 /*------------------------------------
  * /restaurants/:restaurantId/products
  * -----------------------------------*/
+restaurantRouter.route("/:restaurantId/products")
+.get((req, res, next) => {
+    Restaurant.findById(req.params.restaurantId)
+    .then((restaurant) => {
+        if(restaurant != null) {
+            restaurant.products.forEach(product => {
+                product.createdAt -= GMT_Brasil;
+                product.updatedAt -= GMT_Brasil;
+            });
+            
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(restaurant.products);
+        }
+        else {
+            err = new Error("Restaurant " + req.params.restaurantId + " not found!");
+            err.statusCode = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post(authenticate.verifyUser, (req, res, next) => {
+    Restaurant.findById(req.params.restaurantId)
+    .then((restaurant) => {
+        if(restaurant != null) {
+            restaurant.products.push(req.body);
+            restaurant.save()
+            .then((restaurant) => {
+                restaurant.products[restaurant.products.length - 1].createdAt -= GMT_Brasil;
+                restaurant.products[restaurant.products.length - 1].updatedAt -= GMT_Brasil;
 
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json(restaurant.products[restaurant.products.length - 1]);
+            }, (err) => next(err));
+        }
+        else {
+            err = new Error("Restaurant " + req.params.restaurantId + " not found!");
+            err.statusCode = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.put(authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /restaurants/" + req.params.restaurantId + "/products");
+})
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Restaurant.findById(req.params.restaurantId)
+    .then((restaurant) => {
+        if(restaurant != null) {
+            for(var i = (restaurant.products.length - 1); i >= 0; i--) {
+                restaurant.products.id(restaurant.products[i]._id).remove();
+            }
+            restaurant.save()
+            .then((restaurant) => {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json(restaurant.products);
+            }, (err) => next(err));
+        }
+        else {
+            err = new Error("Restaurant " + req.params.restaurantId + " not found!");
+            err.statusCode = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+/*-------------------------------------------------
+ * /restaurants/:restaurantId/products/:productId
+ * ------------------------------------------------*/
+restaurantRouter.route("/:restaurantId/products/:productId")
+.get((req, res, next) => {
+    Restaurant.findById(req.params.restaurantId)
+    //.populate("products.comments")
+    .then((restaurant) => {
+        if(restaurant != null && restaurant.products.id(req.params.productId) != null) {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(restaurant.products.id(req.params.productId));
+        }
+        else if(restaurant == null) {
+            err = new Error("Restaurant " + req.params.restaurantId + " not found!");
+            err.statusCode = 404;
+            return next(err);
+        }
+        else {
+            err = new Error("Product " + req.params.productId + " not found!");
+            err.statusCode = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    res.statusCode = 403;
+    res.end("POST method not supported on /restaurants/" + req.params.restaurantId + "/products/" + req.params.productId);
+})
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Restaurant.findById(req.params.restaurantId)
+    .then((restaurant) => {
+        if(restaurant != null && restaurant.products.id(req.params.productId) != null) {
+            if(req.body.name  != null) restaurant.products.id(req.params.productId).name  = req.body.name;
+            if(req.body.price != null) restaurant.products.id(req.params.productId).price = req.body.price;
+            if(req.body.type  != null) restaurant.products.id(req.params.productId).type  = req.body.type;
+            if(req.body.image != null) restaurant.products.id(req.params.productId).image = req.body.image;
+
+            restaurant.save()
+            .then((restaurant) => {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json(restaurant.products.id(req.params.productId));
+            }, (err) => next(err));
+        }
+        else if(restaurant == null) {
+            err = new Error("Restaurant " + req.params.restaurantId + " not found!");
+            err.statusCode = 404;
+            return next(err);
+        }
+        else {
+            err = new Error("Product " + req.params.productId + " not found!");
+            err.statusCode = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Restaurant.findById(req.params.restaurantId)
+    .then((restaurant) => {
+        if(restaurant != null && restaurant.products.id(req.params.productId) != null) {
+            var removed = restaurant.products.id(req.params.productId).remove();
+            restaurant.save()
+            .then((restaurant) => {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json(removed);
+            }, (err) => next(err));
+        }
+        else if(restaurant == null) {
+            err = new Error("Restaurant " + req.params.restaurantId + " not found!");
+            err.statusCode = 404;
+            return next(err);
+        }
+        else {
+            err = new Error("Product " + req.params.productId + " not found!");
+            err.statusCode = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
 
 module.exports = restaurantRouter;
