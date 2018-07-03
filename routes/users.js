54c8c4578dcadeb3983eebb2aff63e96aa4a801f
DiegoOsmarinBasso <diego.osmarin@acad.pucrs.br>
2018-07-03 17:44:10 -0300

    Retorna _id em signup e login

--- routes/users.js
+++ routes/users.js
@@ -23,7 +23,7 @@ router.post("/signup", (req, res, next) => {
   User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
     if(err) {
-      res.statusCode = 500;
+      res.statusCode = 400;
       res.setHeader("Content-Type", "application/json");
       res.json({err: err});
     }
@@ -31,7 +31,7 @@ router.post("/signup", (req, res, next) => {
       passport.authenticate("local")(req, res, () => {
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
-        res.json({success: true, status: "Registration Successful!"});
+        res.json({success: true, _id: req.user._id, status: "Registration Successful!"});
       });
     }
   });
@@ -42,7 +42,7 @@ router.post("/login", passport.authenticate("local"), (req, res) => {
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.json({success: true, token: token, status: "You are successfully logged in!"});
+  res.json({success: true, _id: req.user._id, token: token, status: "You are successfully logged in!"});
 });
 
 // GET /profile
